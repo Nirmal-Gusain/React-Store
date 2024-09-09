db53b5b72ece799544d329d84d5fe9e1e15b7052
Nirmal Gusain <155016618+Nirmal-Gusain@users.noreply.github.com>
2024-09-09 11:47:06 +0530

    Daisy UI configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 function App() {
   return (
-    <h1 className='bg-slate-800 text-white'>hello</h1>
-  )
+    <div className="card bg-base-100 w-96 shadow-xl">
+      <figure className="px-10 pt-10">
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+          alt="Shoes"
+          className="rounded-xl"
+        />
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">Run in Pegasus 41</h2>
+        <p className="text-2xl font-extrabold">Don’t Waste Your Energy</p>
+        <div className="card-actions">
+          <button className="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
